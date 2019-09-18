2c1d174ab13cc6a95eb3931c4221df75cc459ecc
Samuel Xavier <sancrisxa@gmail.com>
2019-09-18 19:41:51 -0300

    Adicionado os teste do update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -30,6 +30,22 @@ class UserController extends Controller
         return $user;
     }
 
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'email' => 'required|unique:users|max:255',
+            'password' => 'required|max:255'
+        ]);
+
+        $user = User::find($id);
+        $user->name = $request->input('name');
+        $user->name = $request->input('email');
+        $user->name = $request->input('password');
+        $user->update();
+        return $user;
+    }
+
     public function view($id)
     {
         return User::find($id);
--- tests/UserTest.php
+++ tests/UserTest.php
@@ -50,5 +50,31 @@ class UserTest extends TestCase
 
     }
 
+    public function testUpdateUser()
+    {
+        $user = \App\User::first();
+        $dados = [
+            'name' => 'Nome 03',
+            'email' => 'email03@email.com',
+            'password' => '123',
+        ];
+
+        $this->put('/api/user/' . $user->id, $dados);
+
+        $this->assertResponseOK();
+
+        $resposta = (array) json_decode($this->response->content());
+
+        $this->assertArrayHasKey('name', $resposta);
+        $this->assertArrayHasKey('email', $resposta);
+        $this->assertArrayHasKey('id', $resposta);
+
+        $this->notSeeInDatabase('users', [
+            'name' => $user->name,
+            'email' => $user->email,
+            'id' => $user->id,
+
+        ]);
+    }
 
 }
